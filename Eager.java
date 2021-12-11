67fc8d5ea59a6b245adbcfe9865c2f0e70e625e6
aiazk <95961551+aiazk@users.noreply.github.com>
2021-12-11 13:16:11 +0300

    Add files via upload

---
+++ Eager.java
@@ -0,0 +1,29 @@
+
+public class Eager
+{
+	public static void main(String[] args) {
+		SingleObject object = SingleObject.getInstance();
+        object.addCoins(3);
+        object.decrease(2);
+	}
+}
+
+
+class SingleObject {
+   private int coins;
+   private static SingleObject instance = new SingleObject(3);
+   private SingleObject(int c){
+       coins=c;
+   }
+   public static SingleObject getInstance(){
+      return instance;
+   }
+   public void addCoins(int c){
+       coins=coins+c;
+       System.out.println("The number of coins is "+coins);
+   }
+   public void decrease(int c){
+       coins=coins-c;
+       System.out.println("The number of coins is "+coins);
+   }
+}
---
+++ Lazy.java
@@ -0,0 +1,31 @@
+public class Main
+{
+	public static void main(String[] args) {
+		SingleObject object = SingleObject.getInstance();
+        object.addCoins(3);
+        object.decrease(2);
+	}
+}
+
+
+class SingleObject {
+   private int coins;
+   private static SingleObject instance;
+   private SingleObject(int c){
+       coins=c;
+   }
+   public static SingleObject getInstance(){
+        if(instance == null){
+            instance = new SingleObject(3);
+        }
+        return instance;
+    }
+   public void addCoins(int c){
+       coins=coins+c;
+       System.out.println("The number of coins is "+coins);
+   }
+   public void decrease(int c){
+       coins=coins-c;
+       System.out.println("The number of coins is "+coins);
+   }
+}
